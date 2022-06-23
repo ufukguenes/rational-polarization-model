2aea968dfc70fc5e12b9e8b5cd72cd1a96339e81
ufukguenes <u.guenes@live.de>
2022-06-24 00:51:10 +0200

    checkstyle

--- sample/main.py
+++ sample/main.py
@@ -4,8 +4,6 @@ import DeliberationStrategy as delib
 import ArgumentPoolInitialisationStrategy as argp
 from datetime import datetime
 
-
-
 if __name__ == '__main__':
     start = datetime.now()
     # su.standard_set_up(argp.exponential_distribution_pool, forg.unlimited_minded, delib.pure_deliberation, max_steps=10000)
