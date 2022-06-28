9d6db0826d5b130ea7369e1cb683f01ab371c146
ufukguenes <u.guenes@live.de>
2022-06-28 16:15:46 +0200

    changed standard setup

--- sample/main.py
+++ sample/main.py
@@ -6,12 +6,12 @@ from datetime import datetime
 
 if __name__ == '__main__':
     start = datetime.now()
-    #su.standard_set_up(argp.exponential_distribution_pool, forg.coherence_minded, delib.rational_deliberation_simple, max_steps=5000)
+    su.standard_set_up(argp.exponential_distribution_pool, forg.coherence_minded, delib.outside_deliberation)
 
-    #su.statistical_set_up(argp.exponential_distribution_pool, forg.weight_coherence_minded,
-    #                                      delib.outside_deliberation, runs=1000)
+    #su.statistical_set_up(argp.exponential_distribution_pool, forg.coherence_minded,
+    #                                     delib.outside_deliberation, runs=100)
 
 
-    su.statistical_grouped_group_interaction(argp.exponential_distribution_pool, forg.coherence_minded, delib.outside_deliberation,
-                                            talk_show=True, expert_group=True, runs=10, max_steps=10000)
+   # su.statistical_grouped_group_interaction(argp.exponential_distribution_pool, forg.coherence_minded, delib.outside_deliberation,
+    #                                        talk_show=True, expert_group=True, runs=10, max_steps=10000)
     print("process finished in: " + str(datetime.now() - start))
