5d919d22c2597f16db34106ea97aca1cd77ac73f
ufukguenes <u.guenes@live.de>
2022-11-13 13:06:27 +0100

    setup

--- sample/main.py
+++ sample/main.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 if __name__ == '__main__':
     start = datetime.now()
-    #su.standard_set_up(argp.exponential_distribution_pool, forg.unlimited_minded, delib.pure_subset_deliberation)
+    #su.standard_set_up(argp.exponential_distribution_pool, forg.coherence_minded, delib.outside_deliberation)
 
     #su.statistical_set_up(argp.exponential_distribution_pool, forg.unlimited_minded,
      #                                    delib.pure_subset_deliberation, runs=100)
